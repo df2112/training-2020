281f2234fa1b8ee4221f71ca772c9566252dc919
0bie <oegwim@mobify.me>
2020-10-15 13:23:49 -0700

    Initial commit

---
+++ app/connector.js
@@ -0,0 +1,27 @@
+
+import {SalesforceConnector} from '@mobify/commerce-integrations/dist/connectors/sfcc'
+
+// Replace Mobify's demo config with the correct values for your backend:
+export const getConnector = () => {
+    return SalesforceConnector.fromConfig({
+        basePath: `${
+            typeof window === 'undefined' ? process.env.APP_ORIGIN : ''
+        }/mobify/proxy/api/s/RefArch/dw/shop/v20_4`,
+        defaultHeaders: {
+            'x-dw-client-id': 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+        }
+    })
+}
+
+// Return any connector-specific constants for root category ids, etc.
+export const getRootCategoryId = () => {
+    return 'root'
+}
+
+export const getCarouselImageSizeType = () => {
+    return 'large'
+}
+
+export const getCarouselImagePropertyVariation = () => {
+    return 'color'
+}
