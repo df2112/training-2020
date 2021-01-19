00522ac1a9a2bcbd217eeb2a93405e1375aef981
Dave Foley <34948351+df2112@users.noreply.github.com>
2021-01-19 16:10:52 -0500

    Refactor PrescriptionsGrid to function component

--- app/components/prescriptions-grid/index.jsx
+++ app/components/prescriptions-grid/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import Button from 'progressive-web-sdk/dist/components/button'
-import {getAnalyticsManager} from '../../analytics'
+import { getAnalyticsManager } from '../../analytics'
 
 const analyticsManager = getAnalyticsManager()
 const EMAIL_SUBSCRIBE_FORM_NAME = 'email-subscribe'
@@ -15,26 +15,22 @@ export const validate = (values) => {
     return errors
 }
 
-class PrescriptionsGrid extends React.PureComponent {
-    constructor(props) {
-        super(props)
-        this.state = {value: '', error: null}
-        this.handleSubmit = this.handleSubmit.bind(this)
-        this.handleChange = this.handleChange.bind(this)
-    }
-    handleChange(event) {
-        this.setState({value: event.target.value})
-    }
+const PrescriptionsGrid = (props) => {
+    const [value, setValue] = useState('')
+    const [error, setError] = useState(false)
 
-    handleSubmit(event) {
-        const {onSubmit} = this.props
-        const error = validate({email: this.state.value}).email
-        if (error) {
+    const handleChange = (event) => setValue(event.target.value)
+
+    const handleSubmit = (event) => {
+        const { onSubmit } = props
+        const emailError = validate({ email: value }).email
+
+        if (emailError) {
             event.preventDefault()
-            this.setState({error})
+            setError(emailError)
             analyticsManager.track('error', {
                 name: 'PrescriptionsGrid_form',
-                content: error
+                content: emailError
             })
             return
         }
@@ -42,66 +38,54 @@ class PrescriptionsGrid extends React.PureComponent {
         if (onSubmit) onSubmit()
     }
 
-    render() {
-        return (
-            <form
-                id={EMAIL_SUBSCRIBE_FORM_NAME}
-                className="c-email-subscribe"
-                data-analytics-name={EMAIL_SUBSCRIBE_FORM_NAME}
-                onSubmit={this.handleSubmit}
-            >
-                <div className="c-email-subscribe__form-field-row u-flexbox">
-                    <div
-                        className={`c-email-subscribe__form-field u-flex ${
-                            this.state.error ? 'c-email-subscribe__form-field-error' : ''
-                        }`}
-                    >
-                        <div className="c-email-subscribe__form-field-inner">
-                            <div className="c-email-subscribe__form-field-label-wrap">
-                                <label
-                                    className="c-email-subscribe__form-field-label"
-                                    htmlFor="email"
-                                >
-                                    {'Email Address'}
-                                </label>
-                            </div>
-                            <div className="c-email-subscribe__form-field-input">
-                                <input
-                                    id="email"
-                                    type="email"
-                                    data-analytics-name="email"
-                                    className="u-flex"
-                                    required
-                                    onChange={this.handleChange}
-                                    value={this.state.value}
-                                />
-                            </div>
-                            {this.state.error && (
-                                <div className="c-email-subscribe__form-field-error-text">
-                                    {this.state.error}
-                                </div>
-                            )}
+    return (
+        <form id={EMAIL_SUBSCRIBE_FORM_NAME} 
+            className="c-email-subscribe" 
+            data-analytics-name={EMAIL_SUBSCRIBE_FORM_NAME}
+            onSubmit={handleSubmit}
+        >
+            <div className="c-email-subscribe__form-field-row u-flexbox">
+                <div className={`c-email-subscribe__form-field u-flex ${error ? 'c-email-subscribe__form-field-error' : ''}`}>
+                    <div className="c-email-subscribe__form-field-inner">
+                        <div className="c-email-subscribe__form-field-label-wrap">
+                            <label className="c-email-subscribe__form-field-label" htmlFor="email">
+                                {'Email Address'}
+                            </label>
                         </div>
-                    </div>
-                    <div className="c-email-subscribe__form-field c-email-subscribe__button u-flex-none u-margin-start-0">
-                        <div className="c-email-subscribe__form-field-inner">
-                            <div className="c-email-subscribe__form-field-input">
-                                <div
-                                    className="c-email-subscribe__form-field-label"
-                                    aria-hidden="true"
-                                >
-                                    {'Email Address'}
-                                </div>
-                                <Button type="submit" className="pw--primary">
-                                    Submit
-                                </Button>
+                        <div className="c-email-subscribe__form-field-input">
+                            <input
+                                id="email"
+                                type="email"
+                                data-analytics-name="email"
+                                className="u-flex"
+                                required
+                                onChange={handleChange}
+                                value={value}
+                            />
+                        </div>
+                        {error && (
+                            <div className="c-email-subscribe__form-field-error-text">
+                                {error}
                             </div>
+                        )}
+                    </div>
+                </div>
+                <div className="c-email-subscribe__form-field c-email-subscribe__button u-flex-none u-margin-start-0">
+                    <div className="c-email-subscribe__form-field-inner">
+                        <div className="c-email-subscribe__form-field-input">
+                            <div className="c-email-subscribe__form-field-label" aria-hidden="true">
+                                {'Email Address'}
+                            </div>
+
+                            <Button type="submit" className="pw--primary">
+                                Submit
+                            </Button>
                         </div>
                     </div>
                 </div>
-            </form>
-        )
-    }
+            </div>
+        </form>
+    )
 }
 
 PrescriptionsGrid.propTypes = {
