37aa1d692cc0ddf60eee5fd0320d3bcb3adb93d2
Dave Foley <34948351+df2112@users.noreply.github.com>
2021-01-29 14:58:37 -0500

    Prescription Configure WIP

--- app/components/makana/prescription-configure/index.jsx
+++ app/components/makana/prescription-configure/index.jsx
@@ -2,62 +2,22 @@ import React, { Fragment, useRef, useState } from 'react'
 import PropTypes from 'prop-types'
 
 import Button from 'progressive-web-sdk/dist/components/button'
-import List from 'progressive-web-sdk/dist/components/list'
-import ListTile from 'progressive-web-sdk/dist/components/list-tile'
 import { getAnalyticsManager } from '../../../analytics'
 
 const analyticsManager = getAnalyticsManager()
 const DOCTOR_SEARCH_FORM_NAME = 'prescription-configure'
 
-const fakeDoctors = [
-    {
-        _doctorKey: '601',
-        name: 'Fake Doctor 1',
-        age: 99,
-    },
-    {
-        _doctorKey: '602',
-        name: 'Fake Doctor 2',
-        age: 40,
-    },
-    {
-        _doctorKey: '603',
-        name: 'Fake Doctor 3',
-        age: 41,
-    }
-]
-
 const PrescriptionConfigure = (props) => {
     const { onPrescriptionConfigureSubmit } = props
     const [error, setError] = useState(false)
-    const [doctorSearchResults, setDoctorSearchResults] = useState([])
 
     const handleDoctorSearchSubmit = (event) => {
         console.log('DoctorSearch: handleDoctorSearchSubmit()')
 
         const formData = new FormData(event.target)
-        // TODO: Validate formData
-
-        // TODO: 
-        // Fix this filter of fakeDoctors against the input search fields
-        //
-        // Need to update all of the various hardcoded doctors arrays in
-        // all components/pages so that they have fields for city, state, zip etc
-        //
-
-        const doctorSearchResults = fakeDoctors.filter(el => 
-            el._doctorKey == formData.get('name') ||
-            el.name == formData.get('name') ||
-            el.age == formData.get('name'))
 
         event.preventDefault() //TODO: This needs to be reviewed
-        setDoctorSearchResults(doctorSearchResults)
-    }
-
-    const handleDoctorSelectSubmit = (doctorId) => {
-        console.log('DoctorSearch: handleDoctorSelectSubmit()')
-        
-        if (onPrescriptionConfigureSubmit) onPrescriptionConfigureSubmit('yoyo')
+        if (onPrescriptionConfigureSubmit) onPrescriptionConfigureSubmit('yoyo2')
     }
 
     return (
@@ -161,27 +121,6 @@ const PrescriptionConfigure = (props) => {
                     </div>
                 </div>
             </div>
-
-            {/* Fake Doctor Search Results */}
-            <List>
-                {doctorSearchResults.map((item) => (
-                    <ListTile
-                        className="pw--instructional-block"
-                        key={item._doctorKey}
-                        endAction={
-                            <Button 
-                                className="pw--blank" 
-                                icon="trash" 
-                                onClick={() => handleDoctorSelectSubmit(item._doctorKey)} 
-                            />
-                        }
-                    >
-                        <span>
-                            {item.name} : {item.age}
-                        </span>
-                    </ListTile>
-                ))}
-            </List>
 
         </form>
     )
