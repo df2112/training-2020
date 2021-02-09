ab81954c3a3e10ca15f64bb652a3fc6e86e7b395
Dave Foley <34948351+df2112@users.noreply.github.com>
2021-02-09 14:08:21 -0500

    wip

--- app/components/makana/view-models/ViewModels.js
+++ app/components/makana/view-models/ViewModels.js
@@ -1,5 +1,86 @@
 const ViewModels = {
 
+    doctorSearch: [
+        {
+            className: 'masterId_2112',
+            isSimple: true,
+            isfull: true,
+            imageProps: {
+                src:
+                    'https://www.grxstatic.com/d4fuqqd5l3dbz/products/DrugItem_26204.JPG',
+                width: '88px',
+                height: '88px',
+                alt: 'cat'
+            },
+            href: '#',
+            options: [
+                {
+                    label: 'Dave ID (a)'
+                }
+            ],
+            title: 'Atorvastatin (generic Lipitor) 2112',
+            price: '$25'
+        },
+        {
+            className: 'masterId_2112',
+            isSimple: true,
+            imageProps: {
+                src:
+                    'https://www.grxstatic.com/d4fuqqd5l3dbz/products/tms/DrugItem_6931.JPG',
+                width: '88px',
+                height: '88px',
+                alt: 'cat'
+            },
+            href: '#',
+            options: [
+                {
+                    label: 'Dave ID (b)'
+                }
+            ],
+            price: '$675',
+            title: 'Lipitor 2112'
+        },
+        {
+            className: 'masterId_8118',
+            isSimple: true,
+            isfull: true,
+            imageProps: {
+                src:
+                    'https://www.grxstatic.com/d4fuqqd5l3dbz/products/DrugItem_26204.JPG',
+                width: '88px',
+                height: '88px',
+                alt: 'cat'
+            },
+            href: '#',
+            options: [
+                {
+                    label: 'Dave ID (x)'
+                }
+            ],
+            title: 'Atorvastatin (generic Lipitor) 8118',
+            price: '$125'
+        },
+        {
+            className: 'masterId_8118',
+            isSimple: true,
+            imageProps: {
+                src:
+                    'https://www.grxstatic.com/d4fuqqd5l3dbz/products/tms/DrugItem_6931.JPG',
+                width: '88px',
+                height: '88px',
+                alt: 'cat'
+            },
+            href: '#',
+            options: [
+                {
+                    label: 'Dave ID (y)'
+                }
+            ],
+            price: '$1675',
+            title: 'Lipitor 8118'
+        }
+    ],
+
     drugSearch: [
         {
             className: 'masterId_2112',
