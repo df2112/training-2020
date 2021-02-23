dda79c2af9c0664b113e6be427ae195f0edb373d
Dave Foley <34948351+df2112@users.noreply.github.com>
2021-02-22 20:52:34 -0500

    buttons footer wip

--- app/components/footer/index.jsx
+++ app/components/footer/index.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+
+import Button from 'progressive-web-sdk/dist/components/button'
+import IconLabel from 'progressive-web-sdk/dist/components/icon-label'
 
 const year = new Date().getFullYear()
 
-const Footer = ({year}) => (
-    <footer className="c-footer u-padding-top-lg u-padding-bottom-lg">
-        <div className="c-footer__content">
-            <p className="qa-footer__copyright">&copy; {year} Mobify Research & Development Inc.</p>
-        </div>
-    </footer>
-)
+const Footer = ({ year }) => {
+    const [path, setPath] = useState('/')
+
+    return (
+        <footer className="c-footer u-padding-top-sm u-padding-bottom-sm">
+            <div className="c-footer__content">
+                <span style={{ paddingRight: 20 }}>
+                    <Button>
+                        <IconLabel iconName="search" label="Search" />
+                    </Button>
+                </span>
+
+                <span>
+                    <Button>
+                        <IconLabel iconName="store" label="Home" />
+                    </Button>
+                </span>
+
+                <span style={{ paddingLeft: 20 }}>
+                    <Button>
+                        <IconLabel iconName="settings" label="Settings" />
+                    </Button>
+                </span>
+            </div>
+        </footer>
+    )
+}
 
 Footer.defaultProps = {
     year
